6d5620f0f91860cf2dc9c50d4a0a335efcfe8e84
vitaliyvolovnik <51174918+vitaliyvolovnik@users.noreply.github.com>
2022-05-10 17:52:55 +0300

    FirstCommit
    Comit main

---
+++ Freelanser/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+namespace Freelanser.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            SignInManager<IdentityUser> SignInManager;
+            UserManager<IdentityUser> UserManager;
+        }
+    }
+}
---
+++ Freelanser/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Freelanser.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
